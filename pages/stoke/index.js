b15ff29d64ba4181ca5a191305abdfeeaa35b4aa
zack <709577425@qq.com>
2018-12-12 19:08:00 +0800

    stroke complete

--- pages/stoke/index.js
+++ pages/stoke/index.js
@@ -1,4 +1,6 @@
 // pages/stoke/index.js
+const GAME_DATA = require('/js/game_data')
+const Mode = require('/js/mode')
 Page({
 
   /**
@@ -7,22 +9,139 @@ Page({
   data: {
 
   },
+  mode: null,
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    this.mode = new Mode(GAME_DATA[0]);
+    console.log(this.mode.toString());
+   
+    this.data.screenWidth = wx.getSystemInfoSync().windowWidth;
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    this.draw();
+    const that = this;
+    // let time = new Date().getTime();
+    // this.mode.solveSync();
+    // console.log(new Date().getTime()-time);
+    // this.draw();
+    this.mode.solve(()=>{
+      that.draw();
+      return 50;
+    }).then(function(path){
+      
+    });
   },
 
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  draw: function() {
+    const screenWidth = this.data.screenWidth;
+    const padding = screenWidth / 10;
+    const topMargin = 2;
+    var context = wx.createCanvasContext('canvas');
+    const data = this.mode.data;
+    const width = data[0].length;
+    const height = data.length;
+    const item_width = screenWidth / (Math.max(width, height) + 1);
+    const leftMargin = (screenWidth - width * item_width) / 2;
+    const radius = item_width * 0.45;
+    // context.fillRect(leftMargin, topMargin, item_width*width,item_width*height);
+    context.fillStyle = "gray";
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
+        if (data[i][j]) {
+          
+          this.drawDot(context,
+            leftMargin + item_width * j + item_width / 2,
+           topMargin + i * item_width + item_width / 2,
+            radius);
+        }
+      }
+    }
+    const start = this.mode.start;
+    const end = this.mode.end;
+    context.fillStyle = "orange";
+    if(start){
+        this.drawDot(context,
+        item_width*start.x+item_width/2+leftMargin,
+        item_width*start.y+item_width/2+topMargin,radius);
+    }
+    if(end){
+      this.drawDot(context,
+        item_width * end.x + item_width / 2 + leftMargin,
+        item_width * end.y + item_width / 2 + topMargin, radius);
+    }
+
+    //绘制游戏的线条
+
+    // context.fillStyle = "or"
+    let path = this.mode.path;
+    
+    if(path.length>0){
+      context.lineWidth = radius*2;
+      context.beginPath();
+      context.strokeStyle = "orange";
+      this.drawDot(context,
+        item_width * path[0].x + item_width / 2 + leftMargin,
+        item_width * path[0].y + item_width / 2 + topMargin, radius);
+      
+      
+      for (var i = 1; i < path.length; i++) {
+        this.drawDot(context,
+          item_width * path[i].x + item_width / 2 + leftMargin,
+          item_width * path[i].y + item_width / 2 + topMargin, radius);
+        context.beginPath();
+        context.moveTo(leftMargin + path[i-1].x * item_width + item_width/2,
+          topMargin + path[i - 1].y * item_width + item_width / 2)
+        context.lineTo(leftMargin + path[i].x * item_width + item_width / 2,
+          topMargin + path[i].y * item_width + item_width / 2);
+        context.stroke();
+      }
+      context.stroke();
+    }
+    
+
+    //绘制起始/结束表示
+      context.lineWidth = 4;
+    context.beginPath();
+    context.strokeStyle = "white";
+    if (start) {
+      context.moveTo(leftMargin + start.x * item_width+item_width*0.3,
+       topMargin+start.y*item_width+item_width*0.3)
+      context.lineTo(leftMargin + start.x * item_width  + item_width * 0.7,
+       topMargin + start.y * item_width + item_width * 0.7);
+      context.moveTo(leftMargin + start.x * item_width + item_width * 0.7,
+       topMargin + start.y * item_width + item_width * 0.3)
+      context.lineTo(leftMargin + start.x * item_width  + item_width * 0.3,
+       topMargin + start.y * item_width + item_width * 0.7);
+      
+    }
+    if (end) {
+      context.moveTo(leftMargin + end.x * item_width + item_width * 0.3,
+        topMargin + end.y * item_width + item_width * 0.3)
+      context.lineTo(leftMargin + end.x * item_width + item_width * 0.7,
+        topMargin + end.y * item_width + item_width * 0.7);
+      context.moveTo(leftMargin + end.x * item_width + item_width * 0.7,
+        topMargin + end.y * item_width + item_width * 0.3)
+      context.lineTo(leftMargin + end.x * item_width + item_width * 0.3,
+        topMargin + end.y * item_width + item_width * 0.7);
+    }
+    context.stroke();
+    context.draw();
+  },
+  drawDot: function(cxt, x, y, radius) {
+
+    cxt.beginPath();
+    cxt.arc(x, y, radius, 0, Math.PI * 2, true);
+    cxt.closePath();
+    cxt.fill();
   }
 })
--- pages/stoke/js/mode.js
+++ pages/stoke/js/mode.js
@@ -1,4 +1,4 @@
-function Mode(arr) {
+function Mode(arr, start, end) {
   if (arr === undefined) {
     arr = new Array(9);
     for (var i = 0; i < 9; i++) {
@@ -7,17 +7,115 @@ function Mode(arr) {
         arr[i][j] = 0
       }
     }
-  } else
-    if (typeof arr === "string") {
-      arr = str2Array(arr);
-    }
-  this.shudu = arr;
-  this.b = new Array(81);
-  for (var i = 0; i < 81; i++) {
-    this.b[i] = new Model();
+    this.data = arr;
+  } else if (typeof arr === "string") {
+    arr = str2Array(arr);
+    let start = arr.start;
+
+    this.start = start[0];
+    this.end = start[1];
+    this.data = arr.data;
+    console.log(arr);
+  } else {
+    this.data = arr;
+    this.start = start;
+    this.end = end;
   }
+  //处理起点
+  this.path = [];
+  this.start = null;
+  this.end = null;
+  if (!this.start) { //没有起始点，我就找一个，找不到，就随便搞一个
+    let data = this.data;
+
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].length; j++) {
+        if (isOddPoint(data, j, i) == 1) {
+          this.start = {
+            x: j,
+            y: i
+          }
+          console.log(j,i);
+          break;
+        }
+      }
+    }
+
+    if (!this.start) {
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+          if (data[i][j]) {
+            this.start = {
+              x: j,
+              y: i
+            }
+            console.log(j, i);
+            break;
+          }
+        }
+      }
+    }
+  }
+  if (this.end) {
+    let e = this.end;
+    if (isOddPoint(this.data, e.x, e.y) == 1) {
+      this.end = this.start;
+      this.start = e;
+    }
+  }
+
+  // let e = this.end;
+  // this.end = this.start;
+  // this.start = e;
+
 }
-Mode.prototype.cal = function () {
+Mode.prototype.solveSync = function() {
+  let path = [];
+  gameProcess(this.data, this.start, this.end, path);
+  this.path = path;
+  return path;
+}
+Mode.prototype.solve = function(cb) {
+  const that = this;
+  let path = [];
+  return gameProcess2(this.data, this.start, this.end, path, function(data, p) {
+    that.path = path;
+    if (cb) return cb();
+    return 0;
+  });
+}
+Mode.prototype.toString = function() {
+  //●
+  let data = this.data;
+  let sb = "╔";
+  for (let i = 0; i < data[0].length; i++) {
+    sb += "══"
+  }
+  sb += "═╗\n";
+  for (let i = 0; i < data.length; i++) {
+    sb += "║";
+    for (let j = 0; j < data[i].length; j++) {
+      if (data[i][j]) {
+        if ((this.start && this.start.x) == j && (this.start && this.start.y) == i ||
+          (this.end && this.end.x) == j && (this.end && this.end.y) == i) {
+          sb += " ○";
+        } else {
+          sb += " ●" //●//○
+        }
+        // sb += " ●"//●//○
+      } else {
+        sb += "  "
+      }
+    }
+    sb += " ║\n";
+  }
+  sb += "╚";
+  for (let i = 0; i < data[0].length; i++) {
+    sb += "══"
+  }
+  sb += "═╝\n";
+  return sb;
+
 }
 
 function str2Array(str) {
@@ -35,9 +133,326 @@ function str2Array(str) {
       } else if (temp2 == 1) {
         a3[j] = true;
       } else if (temp2 == "x") {
-        start.push({ x: i, y: j });
+        a3[j] = true;
+        start.push({
+          x: j,
+          y: i
+        });
       }
     }
   }
-  return {data:a2,start};
+  return {
+    data: a2,
+    start
+  };
+}
+
+module.exports = Mode;
+var sleep = function(time) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      // 返回 ‘ok’
+      resolve('ok');
+    }, time);
+  })
+};
+async function gameProcess2(arr, start, end, path, cb) {
+  path.push(start);
+  // console.log(path);
+  //绘制path
+
+
+  var x = start.x;
+  var y = start.y;
+  arr[y][x] = false;
+  // await reloadCanvas();
+  await sleep(cb(arr, path))
+  if (end && end.x == x && end.y == y) {
+    return checkOk(arr);
+  }
+  //如果不连通，直接return false;
+  if (!checkOk2(arr, x, y, end)) {
+    return false;
+  }
+  console.log(arr);
+  if (checkOk(arr)) {
+    return true
+  }
+  if (x > 0 && arr[y][x - 1]) {
+    arr[y][x - 1] = false;
+    if (!checkAroundOddPoint(arr, x, y, end)) {
+      var p = await gameProcess2(arr, {
+        x: x - 1,
+        y
+      }, end, path, cb);
+      if (p) {
+        return true;
+      } else {
+        arr[y][x - 1] = true;
+        path.splice(path.length - 1, 1);
+        // await reloadCanvas();
+      }
+    } else {
+      arr[y][x - 1] = true;
+    }
+
+
+
+  }
+
+  if (x < arr[y].length - 1 && arr[y][x + 1]) {
+    arr[y][x + 1] = false;
+    if (!checkAroundOddPoint(arr, x, y, end)) {
+
+      var p = await gameProcess2(arr, {
+        x: x + 1,
+        y
+      }, end, path, cb);
+      if (p) {
+
+        return true;
+      } else {
+        arr[y][x + 1] = true;
+        path.splice(path.length - 1, 1);
+        // await reloadCanvas();
+      }
+    } else {
+      arr[y][x + 1] = true;
+    }
+  }
+
+  if (y > 0 && arr[y - 1][x]) {
+
+
+    arr[y - 1][x] = false;
+    if (!checkAroundOddPoint(arr, x, y, end)) {
+      var p = await gameProcess2(arr, {
+        x,
+        y: y - 1
+      }, end, path, cb);
+      if (p) {
+        return true;
+      } else {
+        arr[y - 1][x] = true;
+        path.splice(path.length - 1, 1);
+        // await reloadCanvas();
+      }
+    } else {
+      arr[y - 1][x] = true;
+    }
+  }
+
+  if (y < arr.length - 1 && arr[y + 1][x]) {
+    arr[y + 1][x] = false;
+    if (!checkAroundOddPoint(arr, x, y, end)) {
+      var p = await gameProcess2(arr, {
+        x,
+        y: y + 1
+      }, end, path, cb);
+      if (p) {
+        return true;
+      } else {
+        arr[y + 1][x] = true;
+        path.splice(path.length - 1, 1);
+      }
+    } else {
+      arr[y + 1][x] = true;
+    }
+  }
+
+  // arr[y][x] = true;
+  // path.splice(len, 1);
+  // await reloadCanvas();
+  // 走到了尽头了
+  return false;
+
+
+
+}
+
+function gameProcess(arr, start, end, path) {
+  path.push(start);
+  // console.log(path);
+  //绘制path
+
+
+  var x = start.x;
+  var y = start.y;
+  arr[y][x] = false;
+  // await reloadCanvas();
+  if (end && end.x == x && end.y == y) {
+    return checkOk(arr);
+  }
+  //如果不连通，直接return false;
+  if (!checkOk2(arr, x, y)) {
+    return false;
+  }
+  if (checkOk(arr)) {
+    return true
+  }
+  if (x > 0 && arr[y][x - 1] && !checkAroundOddPoint(arr, x - 1, y)) {
+    //增加判断 向这个方向移动后，会不会使得周边的点变得孤立，及联通性为1，并且这个点不是end点
+
+    var p = gameProcess(arr, {
+      x: x - 1,
+      y
+    }, end, path);
+    if (p) {
+      return true;
+    } else {
+      arr[y][x - 1] = true;
+      path.splice(path.length - 1, 1);
+      // await reloadCanvas();
+    }
+  }
+
+  if (x < arr[y].length - 1 && arr[y][x + 1] && !checkAroundOddPoint(arr, x + 1, y)) {
+    var p = gameProcess(arr, {
+      x: x + 1,
+      y
+    }, end, path);
+    if (p) {
+
+      return true;
+    } else {
+      arr[y][x + 1] = true;
+      path.splice(path.length - 1, 1);
+      // await reloadCanvas();
+    }
+  }
+
+  if (y > 0 && arr[y - 1][x] && !checkAroundOddPoint(arr, x, y - 1)) {
+    var p = gameProcess(arr, {
+      x,
+      y: y - 1
+    }, end, path);
+    if (p) {
+      return true;
+    } else {
+      arr[y - 1][x] = true;
+      path.splice(path.length - 1, 1);
+      // await reloadCanvas();
+    }
+  }
+
+  if (y < arr.length - 1 && arr[y + 1][x] && !checkAroundOddPoint(arr, x, y + 1)) {
+    var p = gameProcess(arr, {
+      x,
+      y: y + 1
+    }, end, path);
+    if (p) {
+      return true;
+    } else {
+      arr[y + 1][x] = true;
+      path.splice(path.length - 1, 1);
+    }
+  }
+  // arr[y][x] = true;
+  // path.splice(len, 1);
+  // await reloadCanvas();
+  // 走到了尽头了
+  return false;
+
+
+
+}
+
+function checkOk(arr) { //检查所有点是否都走到
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      if (arr[i][j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function checkOk2(a, x, y, end) { //检查连通性，存在孤立点则不能继续
+  var arr = [];
+  for (var i = 0; i < a.length; i++) {
+    arr.push(a[i].concat());
+  }
+  // if(end){
+  //   arr[end.y][end.x] = false;
+  // }
+  _checkOk2(arr, y, x);
+  return checkOk(arr);
+}
+
+function _checkOk2(arr, y, x) {
+  arr[y][x] = false;
+  if (x > 0 && arr[y][x - 1]) {
+    _checkOk2(arr, y, x - 1)
+  }
+
+  if (x < arr[y].length - 1 && arr[y][x + 1]) {
+    _checkOk2(arr, y, x + 1);
+  }
+
+  if (y > 0 && arr[y - 1][x]) {
+    _checkOk2(arr, y - 1, x);
+  }
+
+  if (y < arr.length - 1 && arr[y + 1][x]) {
+    _checkOk2(arr, y + 1, x);
+  }
+}
+
+function getChannel(arr, x, y) { //检查点的连通性
+  if (!arr[y][x]) return 0;
+  let count = 0;
+  if (x > 0 && arr[y][x - 1]) {
+    count++;
+  }
+
+  if (x < arr[y].length - 1 && arr[y][x + 1]) {
+    count++;
+  }
+
+  if (y > 0 && arr[y - 1][x]) {
+    count++;
+  }
+
+  if (y < arr.length - 1 && arr[y + 1][x]) {
+    count++;
+  }
+  return count;
+}
+
+function isOddPoint(arr, x, y, end) { //是只有一个出路的点
+  if (end && end.x == x && end.y == y) {
+    return 0
+  }
+  return getChannel(arr, x, y) == 1;
+}
+var count = 0;
+
+function checkAroundOddPoint(arr, x, y, end) {
+  // return false;
+
+  let b = _checkAroundOddPoint(arr, x, y, end);
+  return b;
+}
+
+function _checkAroundOddPoint(arr, x, y, end) {
+    if(!end)return false;
+
+  //  return false;
+  if (x > 0 && isOddPoint(arr, x - 1, y, end)) {
+    return true;
+  }
+
+  if (x < arr[y].length - 1 && isOddPoint(arr, x + 1, y, end)) {
+    return true;
+  }
+
+  if (y > 0 && isOddPoint(arr, x, y - 1, end)) {
+    return true;
+  }
+
+  if (y < arr.length - 1 && isOddPoint(arr, x, y + 1, end)) {
+    return true;
+  }
+  return false;
 }
