56e66c3d0e6337c36b2635f81b5ba92ec2d50d63
zack <709577425@qq.com>
2019-03-11 03:49:44 +0800

    年龄计算需要修改

--- pages/user/user.js
+++ pages/user/user.js
@@ -16,16 +16,35 @@ Page({
     // })
   },
   onLoad: function () {
-    let users = this.data.users;
-    let user = {}
-    user.name = "张志";
-    user.birth = "1991-10-28"
-    user.age = Util.age(new Date(user.brith))
-    let date2 = new Date(user.birth);
-    console.log(Calendar.getLunarByBetween(1991,9,28));//需要月份+1
-
-    user.age2 = Util.age2(new Date(user.birth))
-    users.push(user);
+    
+    let users = [
+      { name: "乔风云", birth: "1966-4-18" },
+      {name:"张志",birth:"1991-10-28"},
+      { name: "熊卫", birth: "1990-12-20",birth2:"1989-12-20" },
+      
+      { name: "张松山", birth: "1967-5-30" },
+      {name: "张橙", birth: "2018-9-16" },
+      {name: "熊志强", birth: "1968-4-17" },
+      {name: "杨立群", birth: "1972-12-15" },
+      ];
+   let date = new Date();
+   var luncheYear = Calendar.solarToLunar(date.getFullYear(), date.getMonth() + 1, date.getDate()).lunarYear;
+    users.forEach(user=>{
+      date = new Date(user.birth);
+      user.age = Util.age(date);
+      let b2;
+      console.log(user.name,date);
+      if(!user.birth2) {
+         b2= Calendar.solarToLunar(date.getFullYear(), date.getMonth() + 1, date.getDate());
+      } else {
+        date = new Date(user.birth2);
+        let { year: y, month: m, day: d } = Calendar.lunarToSolar(date.getFullYear(), date.getMonth() + 1, date.getDate());
+        b2 = Calendar.solarToLunar(y,m,d);
+        console.log(b2);
+      }
+      user.birth_desc = b2.lunarMonthName + b2.lunarDayName + " " + b2.zodiac;
+      user.age2 = luncheYear - b2.lunarYear + 1;
+    });
 
     
     this.setData({ users });
